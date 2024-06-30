66b39f24a674f2b463025296ce09fa2caa8e6e31
naman995 <rajputnaman823@gmail.com>
2024-06-30 14:57:17 +0530

    Refactor form submission logic to redirect to thank you page

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,24 +1,3 @@
-// function sendMail(event) {
-//   event.preventDefault();
-//   let parms = {
-//     firstName: document.getElementById("firstName").value,
-//     lastName: document.getElementById("lastName").value,
-//     companyName: document.getElementById("companyName").value,
-//     phone: document.getElementById("phone").value,
-//     email: document.getElementById("email").value,
-//     message: document.getElementById("message").value,
-//   };
-//   emailjs.send("service_h8edw2d", "template_ogmu1l8", parms).then(
-//     function (response) {
-//       console.log("SUCCESS!", response.status, response.text);
-//       alert("Message sent successfully");
-//     },
-//     function (error) {
-//       console.log("FAILED...", error);
-//       alert("Message not sent");
-//     }
-//   );
-// }
 function sendMail(event) {
   event.preventDefault();
 
@@ -35,8 +14,12 @@ function sendMail(event) {
   // Use EmailJS to send the form data
   emailjs.send("service_h8edw2d", "template_ogmu1l8", formObject).then(
     () => {
-      alert("Message sent successfully!");
+      // alert("Message sent successfully!");
+      console.log("Redirecting to thank you page");
       form.reset();
+      window.location.href =
+        "/thankyou.html"; 
+        console.log(window.location.href);// Adjust path as needed
     },
     (error) => {
       alert("Failed to send message. Please try again.");
